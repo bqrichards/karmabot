5ca9382956757f61d63c871c155e92ffba6387d6
Brandon Richards <bqrichards@icloud.com>
2023-09-03 15:39:03 -0500

    Check if guild is in queue

--- src/bot/cogs/cog_guild_scanner.py
+++ src/bot/cogs/cog_guild_scanner.py
@@ -23,8 +23,17 @@ class KarmaBotGuildScannerCog(commands.Cog):
     def __init__(self, bot: KarmaBot):
         self.bot = bot
         self.logger = logging.getLogger(__class__.__name__)
+
+        # TODO Move guild queue logic to seperate class
         self.queue: janus.AsyncQueue[KarmaBotContext] = janus.Queue().async_q
-        self.scanning_guilds: dict[int, GuildScanningRecord] = dict()
+        """Queue of contexts of guilds to be scanned"""
+
+        self.queue_ids: set[int] = set()
+        """IDs of guilds in the queue"""
+
+        self.active_scanning_guilds: dict[int, GuildScanningRecord] = dict()
+        """Dictionary of guilds being actively scanned"""
+
         self.scan_guilds.start()
     
     def cog_unload(self) -> Coroutine[Any, Any, None]:
@@ -41,17 +50,21 @@ class KarmaBotGuildScannerCog(commands.Cog):
         guild_id = ctx.guild.id
 
         # Check if already scanning
-        scanning = guild_id in self.scanning_guilds
+        guild_scanning = guild_id in self.active_scanning_guilds
+        guild_in_queue = guild_id in self.queue_ids
 
-        if scanning:
-            scanning_record = self.scanning_guilds[guild_id]
+        if guild_scanning:
+            scanning_record = self.active_scanning_guilds[guild_id]
             await ctx.send(f'This guild is already being scanned. Progress: {scanning_record.current_channel}/{scanning_record.total_channels} channels')
-            return
-        
-        # Begin scanning / add to queue
-        await self.queue.put(ctx)
-        
-        await ctx.send('This guild has be added to the scanning queue')
+        elif guild_in_queue:
+            # TODO Get amount ahead in queue
+            amount_ahead = -1
+            await ctx.send(f'This guild is already in the queue. Amount ahead in queue: {amount_ahead}')
+        else:
+            # Add guild to scanning queue
+            await self.queue.put(ctx)
+            self.queue_ids.add(guild_id)
+            await ctx.send('This guild has be added to the scanning queue')
 
     @tasks.loop(seconds=1)
     async def scan_guilds(self):
@@ -62,19 +75,20 @@ class KarmaBotGuildScannerCog(commands.Cog):
                 self.logger.warn('ctx.guild is none')
             else:
                 guild_id = ctx.guild.id
+                self.queue_ids.remove(guild_id)
 
                 # Mark this guild as being scanned
-                self.scanning_guilds[guild_id] = GuildScanningRecord(total_channels=0, current_channel=0)
+                self.active_scanning_guilds[guild_id] = GuildScanningRecord(total_channels=0, current_channel=0)
 
                 self.logger.debug(f'Begin scanning guild: {guild_id}')
 
                 # Scan all text channels for reactions
                 text_channels = [channel for channel in ctx.guild.channels if isinstance(channel, TextChannel)]
-                self.scanning_guilds[guild_id].total_channels = len(text_channels)
+                self.active_scanning_guilds[guild_id].total_channels = len(text_channels)
                 guild_karma: dict[int, int] = dict()
                 for channel in text_channels:
-                    self.scanning_guilds[guild_id].current_channel += 1
-                    self.logger.debug(self.scanning_guilds[guild_id])
+                    self.active_scanning_guilds[guild_id].current_channel += 1
+                    self.logger.debug(self.active_scanning_guilds[guild_id])
                     await asyncio.sleep(5) # FIXME remove after adding feature
 
                     async for message in channel.history(limit=200): # TODO remove limit
@@ -92,7 +106,7 @@ class KarmaBotGuildScannerCog(commands.Cog):
 
                 self.logger.debug(f'Done scanning guild {guild_id} -> {guild_karma}')
 
-                self.scanning_guilds.pop(guild_id)
+                self.active_scanning_guilds.pop(guild_id)
 
             self.queue.task_done()
 
