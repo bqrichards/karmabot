71b0f3dab15c8a502c7191d0ffd4fa567dc15cc1
Brandon Richards <bqrichards@icloud.com>
2023-10-14 16:41:15 -0500

    Finish tests for karma_command

--- src/test/test_karma_command.py
+++ src/test/test_karma_command.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+import discord
 import pytest
 from bot.commands.karma_command import karma_command
 from test.mocks import mock_guild, mock_karma_bot_context, mock_karma_store
@@ -35,11 +37,39 @@ async def test_mention_target_member():
     karma_value = 101
 
     ctx = mock_karma_bot_context(mock_guild(guild_id))
-    ctx.bot.store = mock_karma_store()
     ctx.message.author.id = target_id
     ctx.bot.store.get_karma_of_user.return_value = karma_value
     
     await karma_command(ctx, f'<@{target_id}>')
 
     ctx.bot.store.get_karma_of_user.assert_called_with(guild_id, target_id)
+    ctx.reply.assert_called_with(f'{karma_value}')
+
+@pytest.mark.asyncio
+async def test_target_member_not_found():
+    """Test reply with member cannot be found"""
+    guild_id = 4123123
+
+    ctx = mock_karma_bot_context(mock_guild(guild_id))
+    ctx.guild.get_member_named.return_value = None
+    
+    await karma_command(ctx, 'nonExistantUsername')
+
+    ctx.reply.assert_called_with('Could not find a member with that name')
+
+@pytest.mark.asyncio
+async def test_target_member_found():
+    """Test reply with member can be found"""
+    guild_id = 4123123
+    member_id = 31233
+    karma_value = 1222
+
+    ctx = mock_karma_bot_context(mock_guild(guild_id))
+    ctx.guild.get_member_named.return_value = Mock(spec=discord.Member)
+    ctx.guild.get_member_named.return_value.id = member_id
+    ctx.bot.store.get_karma_of_user.return_value = karma_value
+
+    await karma_command(ctx, 'existingUsername')
+
+    ctx.bot.store.get_karma_of_user.assert_called_with(guild_id, member_id)
     ctx.reply.assert_called_with(f'{karma_value}')
